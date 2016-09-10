737501f74797d85bb05998890af652f0ce9150c3
Attila Komaromi <attila.kom@gmail.com>
2016-09-10 13:05:23 -0400

    email val update

--- js/validation.js
+++ js/validation.js
@@ -14,13 +14,6 @@
 			$('#name,#email,#phone,#message').click(function(){
 				$(this).removeClass("error_input");
 			});
-
-
-            // email validation function with RegEx
-            function isEmail(email){
-
-                return /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/.test( email );  
-            }
             
          	// Form field validation
             if(name.length == 0){
@@ -29,7 +22,7 @@
             }else{
                 $('#name').removeClass("error_input");
             }
-            if(email.length == 0 || email.indexOf('@') == '-1' || !isEmail(email)){
+            if(email.length == 0 || email.indexOf('@') == '-1'){
                 var error = true;
                 $('#email').addClass("error_input");
             }else{
